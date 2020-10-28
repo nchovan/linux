94e25532256ca7eb6047fb366b4ef341f65cd45f
Nicholas c <nchovan85@gmail.com>
2020-10-28 12:01:55 -0500

    folders

---
+++ code for not files <.sh
@@ -0,0 +1,9 @@
+sed 's/<//' ~/Documents/notes/quick_cmds > ~/Documents/notes/quick_cmds_test_output.txt
+
+sed '/^[a-z]/G' ~documents/notes/quick_cmds_test_output.txt > ~/Documents/notes/quick_cmds.txt
+
+rm ~/Documents/notes/quick_cmds
+
+
+
+
