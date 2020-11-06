d2d0c96de5a9a053b83b993dc4944710560b9d5b
Nicholas c <nchovan85@gmail.com>
2020-11-06 11:35:55 -0600

    pycharm second

--- python/scipts/pycharm/contact_info.py
+++ python/scipts/pycharm/contact_info.py
@@ -3,19 +3,23 @@ from datetime import date
 # sets variable current_date to today's date
 current_date = date.today()
 
-# sets vaiable name to the user's input
+# sets variable name to the user's input
 name = input('please enter your name: ')
 dob = input('please enter your date of birth (mm-dd-yyyy): ')
 address = input('please enter your address: ')
 email = input('please enter your email address: ')
 
-# sets the varibale age to the current yer minus the birth year from the variable dob, this is done by using the index
+# sets the variable age to the current yer minus the birth year from the variable dob, this is done by using the index
 # position [6:10] (which is the 7th position through the 10th) while using int to set that to an integer for subtraction
 age = current_date.year - int(dob[6:10])
 
+# uses a formatted string (f) to print results in an easier to read code file,
+# this uses place holders {} for the variable
+contact_listing = (f'\nName: {name}'
+                  f'\nDate fo birth: {dob}'
+                  f'\nAge: {str(age)}'
+                  f'\nAddress: {address}'
+                  f'\nEmail: {email}')
 # uses \n to return a new line and then print the word "Name:" followed by the user input defined by the variable name
-print('\nName: ' + name,
-      '\nDate of birth: ' + dob,
-      '\nAge: ' + str(age),   # sets age to print as a string, this prevents error: can only concatenate str (not "int") to str
-      '\nAddress: ' + address,
-      '\nEmail: ' + email)
+
+print(contact_listing)
--- python/scipts/pycharm/test.py
+++
@@ -1,5 +0,0 @@
-from datetime import date
-current_date = date.today()
-dob = input('please enter your date of birth (mm-dd-yyyy): ')
-age = (current_date.year - int(dob[6:10]))
-print("Age: " + age)
