e323f0f38e5ccfb5a059278897f93d6927864e8e
Nicholas c <nchovan85@gmail.com>
2020-11-06 15:24:55 -0600

    added number to text as function

--- python/scipts/pycharm/number_to_text.py
+++ python/scipts/pycharm/number_to_text.py
@@ -1,24 +1,25 @@
-# get user input
-phone = input("phone: ")
+# create function to change numbers to text
+def number_to_text(message):
+    # maps numbers to text
+    digit_mapping = {
+        "0": "zero",
+        "1": "one",
+        "2": "two",
+        "3": "three",
+        "4": "four",
+        "5": "five",
+        "6": "six",
+        "7": "seven",
+        "8": "eight",
+        "9": "nine",
+        }
+    # set empty output string
+    output = ""
+    # this will go through each number the user entered and get the mapped text
+    for char in message:
+        output += digit_mapping.get(char, "!") + " "
+    return output
 
-# maps numbers to text
-digit_mapping = {
-    "0": "zero",
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
-    "5": "five",
-    "6": "six",
-    "7": "seven",
-    "8": "eight",
-    "9": "nine",
-}
-# set empty output string
-output = ""
-# this will go through each number the user entered and get the mapped text
-for char in phone:
-    output += digit_mapping.get(char, "!") + " "
 
-# prints the new text output
-print(output)
+message = input("phone: ")
+print(number_to_text(message))
