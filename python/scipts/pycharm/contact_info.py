838c1d640dc3a5b9095228ff8e2d64478704bb1d
Nicholas c <nchovan85@gmail.com>
2020-11-06 11:49:26 -0600

    changed input text from "your" to "the"

--- python/scipts/pycharm/contact_info.py
+++ python/scipts/pycharm/contact_info.py
@@ -4,10 +4,10 @@ from datetime import date
 current_date = date.today()
 
 # sets variable name to the user's input
-name = input('please enter your name: ')
-dob = input('please enter your date of birth (mm-dd-yyyy): ')
-address = input('please enter your address: ')
-email = input('please enter your email address: ')
+name = input('please enter the name: ')
+dob = input('please enter the date of birth (mm-dd-yyyy): ')
+address = input('please enter the address: ')
+email = input('please enter the email address: ')
 
 # sets the variable age to the current yer minus the birth year from the variable dob, this is done by using the index
 # position [6:10] (which is the 7th position through the 10th) while using int to set that to an integer for subtraction
